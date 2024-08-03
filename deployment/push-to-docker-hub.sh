ff95298a850c87c0706a784c7c41b2d16ddbb181
unknown <wsakib87@gmail.com>
2024-08-03 13:40:15 +0600

    added workflow: docker_image_Create_and_push.yml

--- deployment/push-to-docker-hub.sh
+++ deployment/push-to-docker-hub.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-DOCKER_HUB_USERNAME=""
-DOCKER_HUB_PASSWORD=""
-DOCKER_HUB_IMAGE=""
-REMOTE_DOCKER_HUB_REPO=""
-TAG=""
+# DOCKER_HUB_USERNAME=""
+# DOCKER_HUB_PASSWORD=""
+# LOCAL_DOKER_IMAGE_TAG=""
+# REMOTE_DOCKER_HUB_REPO=""
+TAG="latest"
 
 echo "Login to docker hub"
 echo "$DOCKER_HUB_PASSWORD" | docker login -u $DOCKER_HUB_USERNAME --password-stdin
 
 echo "Building image"
-docker build -t $DOCKER_HUB_IMAGE ..
+docker build -t $LOCAL_DOKER_IMAGE_TAG ..
 
 echo "Tagging image"
-docker tag $DOCKER_HUB_IMAGE $REMOTE_DOCKER_HUB_REPO
+docker tag $LOCAL_DOKER_IMAGE_TAG $REMOTE_DOCKER_HUB_REPO
 
 echo "Pushing image"
-docker push $DOCKER_HUB_IMAGE:$TAG
+docker push $REMOTE_DOCKER_HUB_REPO:$TAG
 
 echo "Done"
