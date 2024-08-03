803c12d5708a9963da4392a055b89f7ea6890d43
unknown <wsakib87@gmail.com>
2024-08-03 14:24:36 +0600

    updated docker image creation and push workflow

--- deployment/push-to-docker-hub.sh
+++ deployment/push-to-docker-hub.sh
@@ -8,6 +8,9 @@ LOCAL_DOKER_IMAGE_TAG="${LOCAL_DOKER_IMAGE_TAG}"
 REMOTE_DOCKER_HUB_REPO="${REMOTE_DOCKER_HUB_REPO}"
 TAG="latest"
 
+echo "$DOCKER_HUB_USERNAME"
+echo "$DOCKER_HUB_PASSWORD"
+
 echo "Login to docker hub"
 echo "$DOCKER_HUB_PASSWORD" | docker login -u $DOCKER_HUB_USERNAME --password-stdin
 
